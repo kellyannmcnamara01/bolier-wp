255e370e4c3bc84b324cddd29cf9a94632d824ea
Kelly <kellyannmcnamara01@gmail.com>
2020-06-01 12:46:48 -0400

    set up primary menu

--- wp-content/themes/context/functions.php
+++ wp-content/themes/context/functions.php
@@ -7,10 +7,12 @@
 
 	// includes
 	include( get_theme_file_path( '/includes/front/enqueue.php' ) );
+	include( get_theme_file_path( '/includes/setup.php' ) );
 
 
 	// hooks
 	add_action( 'wp_enqueue_scripts', 'context_enqueue' );
+	add_action( 'after_setup_theme', 'context_setup_theme' );
 
 
 	// shortcodes
---
+++ wp-content/themes/context/includes/setup.php
@@ -0,0 +1,7 @@
+<?php
+
+	function context_setup_theme(){
+		register_nav_menu( 'primary', __( 'Primary Menu', 'context' ) );
+	}
+
+?>
