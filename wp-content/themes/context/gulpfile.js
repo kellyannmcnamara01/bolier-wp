1df50b5e9c4ac669f23716ae284f0f3e6bcc0d6f
Kelly <kellyannmcnamara01@gmail.com>
2020-06-04 12:22:30 -0400

    gulp

---
+++ wp-content/themes/context/gulpfile.js
@@ -0,0 +1,135 @@
+// https://coder-coder.com/gulp-tutorial-beginners/
+// https://www.hongkiat.com/blog/access-localhost-public-address/
+// https://github.com/gulpjs/gulp/tree/v3.9.1/docs
+// https://medium.com/@warrebuysse/1-the-road-to-senior-gulp-file-include-9793292fbb22
+
+
+
+
+
+/* Initialize packages
+============================================= */
+// *    In order to take advantage of all the features of the npm packages you added to your project, you need to export them as modules in Node. (Hence the folder name “node_modules”)
+var gulp = require('gulp');
+var cssnano = require('gulp-cssnano');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
+
+
+
+/* Local Host
+============================================= */
+// carate const var for localhost url
+const LOCALSERVER = "local.boiler-wp";
+
+
+
+
+// Create your Gulp tasks
+// *    This allows you to run the Gulp task by typing in gulp [Function Name] into the command line. This is important because you can then run that named function from other Gulp tasks.
+// *    Specifically, we are building several different Gulp tasks, which will *all* be run when you run the default Gulp task.
+// *    .task() — Creates a task, as mentioned above
+// *    .src() — identifies what files you will be compiling in a particular task
+// *    .pipe() — adds a function to the Node stream that Gulp is using; you can pipe multiple functions in the same task (read an excellent write-up on this topic on florian.ec)
+// *    .dest() — writes the resulting file to a specific location
+// *    .watch() — identifies the files to detect any changes on
+
+
+
+
+
+/* SCSS Tasks
+============================================= */
+gulp.task('sass', function(){
+    return gulp.src('assets/src/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('assets/dist/css'));
+});
+
+// IE SCSS Task
+gulp.task('iesass', function(){
+    return gulp.src('assets/src/ie-styles/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('assets/dist/css'));
+});
+
+
+
+
+
+/* JS Tasks
+============================================= */
+gulp.task('js', function(){
+    return gulp.src([
+            'assets/src/scripts/jquery-1.11.2.min.js', 
+            'assets/src/scripts/scripts.js'
+        ])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('assets/dist/js'));
+});
+
+
+
+
+/* PHP Task
+============================================= */
+gulp.task('php', function(){
+    return gulp.src()
+});
+
+
+
+
+/* Watch Task
+============================================= */
+// *    The Watch task will watch the files that you tell it to for any changes. Once it detects a change, it will run the tasks that you designate and then continue watching for changes.
+// *    The watch() function takes two parameters: the source location, and then the tasks to run when a change is detected.
+// *    The Sass Watch function will watch any SCSS files in the app folder and then run the Sass task if it detects changes.
+// *    For the JavaScript Watch function, we’ll have to take advantage of a really useful Node feature called “globbing.” Globbing refers to using the “**” symbols as a kind of wildcard for folders and subfolders. We need it for the JS files because we have a JS file in the app/js folder, and a JS file in the app/js/plugins folder.
+gulp.task('watch', function(){
+    browserSync.init({
+        proxy: {
+            target: LOCALSERVER
+        }, 
+        host: 'localhost'
+    });
+
+    gulp.watch('assets/src/styles/**/*.scss', gulp.series('sass'));
+    gulp.watch('assets/src/ie-styles/**/*.scss', gulp.series('iesass'));
+    gulp.watch('assets/src/scripts/**/*.js', gulp.series('js'));
+});
+
+
+
+
+
+/* Deafult Gulp Task
+============================================= */
+// *    The default Gulp task is what you want to run when you just type in gulp in the command line. When you create the task, you have to call it “default” in order for Gulp to recognize that that’s what you want to run.
+// *    What we’d like to do is run the Sass and JS tasks once, then run the Watch task to re-run tasks when files are changed.
+gulp.task('default', gulp.series('sass', 'iesass', 'js', 'watch'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- wp-content/themes/context/includes/front/enqueue.php
+++ wp-content/themes/context/includes/front/enqueue.php
@@ -7,13 +7,13 @@
 
 		// style sheets
 		wp_register_style( 'context_google_font', 'https://fonts.googleapis.com/css2?family=Fira+Sans', [], $ver );
-		wp_register_style( 'context_styles', $uri . '/assets/src/styles/main.css', [], $ver );
+		wp_register_style( 'context_styles', $uri . '/assets/dist/css/main.css', [], $ver );
 
 		wp_enqueue_style( 'context_google_font' );
 		wp_enqueue_style( 'context_styles' );
 
 		//scripts
-		wp_register_script( 'context_scripts', $uri . '/assets/src/scripts/main.js', [], $ver, true );
+		wp_register_script( 'context_scripts', $uri . '/assets/dist/scripts/scripts.js', [], $ver, true );
 
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'context_scripts' );
