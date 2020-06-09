0ea4717a4cae4a69fe51beecfa6dd71e85eaef8b
Kelly <kellyannmcnamara01@gmail.com>
2020-06-09 14:26:20 -0400

    readme

--- wp-content/themes/context/footer.php
+++ wp-content/themes/context/footer.php
@@ -71,20 +71,7 @@
 
             <!-- Back to Top Btn
             ============================================= -->
-            <?php 
-                if( get_theme_mod( 'content_show_top_btn' ) ){ 
-                    ?>
-                        <button class="back-to-top js--back-to-top">
-                            <span>
-                                <span><span class="e-reader-only">back to top of page</span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
-                        </button>
-                    <?php 
-                } 
-            ?>
-            <!-- Back to Top Btn [END] -->
+            <?php get_template_part( 'partials/reusables/back-to-top' ); ?>
 
 
 
---
+++ wp-content/themes/context/partials/reusables/back-to-top.php
@@ -0,0 +1,13 @@
+<?php 
+    if( get_theme_mod( 'content_show_top_btn' ) ){ 
+        ?>
+            <button class="back-to-top js--back-to-top">
+                <span>
+                    <span><span class="e-reader-only">back to top of page</span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+            </button>
+        <?php 
+    } 
+?>
