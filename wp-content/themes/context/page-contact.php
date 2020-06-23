72ec519299603b86ca0ffc288794fa4becd1888f
Kelly <kellyannmcnamara01@gmail.com>
2020-06-23 10:03:04 -0400

    very base form

--- wp-content/themes/context/page-contact.php
+++ wp-content/themes/context/page-contact.php
@@ -14,23 +14,211 @@
 	<section id="main-content" style="padding: 30px; background: chartreuse;">
 
 
+
+
+		<!-- Form
+		=============================================
+		*
+		* Source: https://webaim.org/techniques/forms/controls
+		*
+		* Accessibility Form Notes to Follow
+		*
+		* As suggested on webaim, inputs should not be wrapped in labels, but rather have an id that matches a label's for att.
+		* Form fields collecting certain types of user-specific information need appropriate autocomplete attributes to identify input purpose. Learn More -> https://www.w3.org/TR/WCAG21/#input-purposes 
+		* Applying the aria-required="true" attribute to a form control will cause screen readers to announce "required" along with the label. No validation or "enforcement" mechanism comes along with this attribute, and it has no visual impact.
+		* Fieldset and legend should only be used when a higher-level label is necessary.
+		* Avoid using multiple-select menus
+		* Screen readers announce the nested text for <button> elements, and the value attribute for input buttons. Form buttons must never be empty.
+		* Reset buttons should not be used unless specifically needed, because they are easy to click by mistake. Learn More -> https://www.nngroup.com/articles/reset-and-cancel-buttons/
+		* Image buttons (<input type="image">) must have equivalent alt text. Otherwise, screen reader users will just hear "button", with no indication of what the button does.
+		============================================= -->
+
 		<div class="row">
 			<div class="col-xs-12">
 				<form class="row" style="padding: 30px; background: darkviolet; color: #fff;">
-					<div class="form-field col-xs-6">
+
+
+
+
+					<!-- Text Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
 						<label for="name">Name:</label>
-						<input type="text" name="name" id="name">
+						<input type="text" name="name" id="name" autocomplete="name" required aria-required="true">
 					</div>
-					<div class="form-field col-xs-6">
+
+
+
+
+					<!-- Email Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
 						<label for="email">Email:</label>
-						<input type="email" name="email" id="email">
+						<input type="email" name="email" id="email" autocomplete="email">
 					</div>
-					<div class="form-field col-xs-12">
+
+
+
+
+					<!-- Phone Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="phone">Phone:</label>
+						<input type="tel" name="phone" id="phone" autocomplete="tel">
+					</div>
+
+
+
+
+					<!-- Url Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="url">Url:</label>
+						<input type="url" name="url" id="url">
+					</div>
+
+
+
+
+					<!-- Number Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="number">Number:</label>
+						<input type="number" name="number" id="number">
+					</div>
+
+
+
+
+					<!-- Range Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="range">Range:</label>
+						<input type="range" name="range" id="range">
+					</div>
+
+
+
+
+					<!-- Color Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="color">Color Field:</label>
+						<input type="color" name="color" id="color">
+					</div>
+
+
+
+
+					<!-- Password Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="password">Password:</label>
+						<input type="password" name="password" id="password">
+					</div>
+
+
+
+
+					<!-- Date Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="date">Date:</label>
+						<input type="date" name="date" id="date">
+					</div>
+
+
+
+
+					<!-- Time Input Example
+					============================================= -->
+					<div class="form-field col-xs-4">
+						<label for="time">Time:</label>
+						<input type="time" name="time" id="time">
+					</div>
+
+
+
+
+					<!-- Checkboxes within Fieldset
+					============================================= -->
+					<div class="form-field col-xs-6">
 						<fieldset>
 							<legend>Checkboxes in fieldset</legend>
-							
+							<input type="checkbox" name="red" id="red" value="red">
+							<label for="red">Red</label>
+							<input type="checkbox" name="yellow" id="yellow" value="yellow">
+							<label for="yellow">Yellow</label>
+							<input type="checkbox" name="blue" id="blue" value="blue">
+							<label for="blue">Blue</label>
 						</fieldset>
 					</div>
+
+
+
+
+					<!-- Radios within Fieldset
+					============================================= -->
+					<div class="form-field col-xs-6">
+						<fieldset>
+							<legend>Radios in fieldset</legend>
+							<input type="radio" name="orange" id="orange" value="orange">
+							<label for="orange">Orange</label>
+							<input type="radio" name="green" id="green" value="green">
+							<label for="green">Green</label>
+							<input type="radio" name="purple" id="purple" value="purple">
+							<label for="purple">Purple</label>
+						</fieldset>
+					</div>
+
+
+
+
+					<!-- Select Example
+					============================================= -->
+					<div class="form-field col-xs-6">
+						<label for="colours">Select Example</label>
+						<select id="colours" name="colours">
+							<option value="red">Red</option>
+							<option value="yellow">Yellow</option>
+							<option value="blue">Blue</option>
+							<option value="orange">Orange</option>
+							<option value="green">Green</option>
+							<option value="purple">Purple</option>
+						</select>
+					</div>
+
+
+
+
+					<!-- Select Example with Grouping
+					============================================= -->
+					<div class="form-field col-xs-6">
+						<label for="colours2">Select Example with Grouping</label>
+						<select id="colours2" name="colours2">
+							<optgroup label="Primary">
+								<option value="red">Red</option>
+								<option value="yellow">Yellow</option>
+								<option value="blue">Blue</option>
+							</optgroup>
+							<optgroup label="Secondary">
+								<option value="orange">Orange</option>
+								<option value="green">Green</option>
+								<option value="purple">Purple</option>
+							</optgroup>
+						</select>
+					</div>
+
+
+
+
+					<!-- Buttons
+					============================================= -->
+					<input type="submit" name="submit" value="Submit Search">
+					<input type="reset" name="reset" value="Reset">
+					<button>Activate</button>s
+
+
 				</form>
 			</div>
 		</div>
